569804b4784066fce7a30213d1527883c139cd50
oalieksienko <oalieks@mail.ru>
2016-03-26 13:49:36 +0300

    initial build

---
+++ databases-uni-project/service/src/main/java/oalex/uni/databases/App.java
@@ -0,0 +1,13 @@
+package oalex.uni.databases;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
